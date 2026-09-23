eae0db54365fbc86c895c5a0ea28f6582b566a8d
Lucas Diniz <117389224+zLadz@users.noreply.github.com>
2026-09-23 11:30:22 -0300

    Update CV and more

--- data/projetos.js
+++ data/projetos.js
@@ -1,5 +1,13 @@
 const projetos = [
   {
+    titulo: 'Kotoba',
+    descricao:
+      'Kotoba é um dicionário Japonês → Português Brasileiro.' +
+      'As traduções em pt-BR são uma camada lexical própria. ',
+    link: 'https://github.com/zLadz/Kotoba-Jisho',
+    tipo: 'github',
+  },
+  {
     titulo: 'IkouEngine',
     descricao:
       'Engine de jogos pessoal, desenvolvida para estudo e prática de programação, ' +
